60a8e7df402a5e990470196878e5a0c5ee5fd42d
dansarms <102619706+dansarms@users.noreply.github.com>
2022-12-24 17:11:09 -0300

    Netflix Repository creating

---
+++ public/js/api.js
@@ -0,0 +1,11 @@
+//Chave de uso da API
+let api_key = "fc7c399ec1ae0d79d02cb1a7da6f07bb"
+
+//Link que pega as capas dos filmes
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+//Link que traz os gêneros
+let genre_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+//Link que faz o discover dos filmes
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
